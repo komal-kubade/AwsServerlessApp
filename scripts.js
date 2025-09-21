8841afb82559911c11a996c5e53cebf029bdc032
komal-kubade <kubadekomak900@gmail.com>
2025-09-21 18:57:57 +0530

    2

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Add your API endpoint here
-var API_ENDPOINT = "https://thytfynupf.execute-api.us-east-1.amazonaws.com/prod";
+var API_ENDPOINT = "https://thytfynupf.execute-api.us-east-1.amazonaws.com/prod/";
 
 // AJAX POST request to save student data
 document.getElementById("savestudent").onclick = function(){
